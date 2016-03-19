2003518f277aeca881ab5ee3d20d9885f47f7b4a
arboldesign <francisco.vazquez@arboldesign.com>
2016-03-19 17:25:55 -0600

    todos los archivos

---
+++ index.php
@@ -0,0 +1,265 @@
+<!doctype html>
+<html lang="en">
+	
+	
+<head>
+	
+	
+	<meta charset="UTF-8" />
+	<title>my first bootstrap</title>
+	
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link href="inc/css/bootstrap.css" rel="stylesheet"></link>
+	<link href="inc/css/style2.css" rel="stylesheet"></link>
+	
+	
+	
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+
+
+
+
+</head>
+
+
+
+
+<body>
+	
+
+	
+	
+	
+	<!-- !NAVBAR  -->
+	 
+	 
+	
+	<div class="navbar navbar-inverse navbar-static-top ">
+				<div class="container">
+			
+			<a href="#" class="navbar-brand">Tech site</a>
+			
+			<button class="navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+			
+			
+			<div class="collapse navbar-collapse navHeaderCollapse">
+				
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="#">Blog</a></li>
+					
+					
+					<li class="dropdown">
+					
+					<a href="#" class="dropdown-toggle" data-toggle = "dropdown" >Social Media <b class="caret"></b></a>
+					
+					<ul class="dropdown-menu">
+						<li><a href="#">Twitter</a></li>	
+						<li><a href="#">Facebook</a></li>	
+						<li><a href="#">Google+</a></li>	
+						<li><a href="#">Instagram</a></li>	
+						
+					</ul>
+					
+					</li>
+					
+					
+					
+					
+					<li><a href="about.php">About</a></li>
+					
+					
+					
+					
+					<li><a href="#contact" data-toggle = "modal" >Contact </a></li>
+					
+					
+					
+				</ul>
+				
+				
+			</div>
+			
+		</div>
+		
+		
+		
+		
+	</div>
+	
+
+	 
+	<!-- !END NAVBAR  -->
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- !BEGIN CONTENT SITE  -->
+	
+<div class="container">
+	
+	
+	<div class="jumbotron">
+		
+		<center><h1>hello, World</h1>
+		<p>Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.</p>
+		
+		<a href="#" class="btn btn-danger ">Watch Now</a>
+		
+		<a href="#" class="btn btn-info ">Twee it</a></center>
+		
+		
+	</div>
+	
+</div>	
+	
+	
+	
+	
+	<div class="container">
+		
+		
+		<div class="row">
+			
+			
+			
+			
+			<div class="col-md-3">
+				
+				<h3><a href="#">$500 Gaming PC Build</a></h3>
+				<p>
+					Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur
+					
+				</p>
+				
+				<a href="#" class="btn btn-default ">Watch Now</a>
+				
+			</div>  <!-- cl=COL 1  -->
+			
+			
+			
+			
+			
+			<div class="col-md-3">
+				
+				<h3><a href="#">$500 Gaming PC Build</a></h3>
+				<p>
+					Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur
+					
+				</p>
+				
+				<a href="#" class="btn btn-default ">Watch Now</a>
+				
+			</div>  <!-- cl=COL 1  -->
+			
+			
+			
+			
+			
+				<div class="col-md-3">
+				
+				<h3><a href="#">$500 Gaming PC Build</a></h3>
+				<p>
+					Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur
+					
+				</p>
+				
+				<a href="#" class="btn btn-default ">Watch Now</a>
+				
+			</div>  <!-- cl=COL 1  -->
+			
+			
+			
+			
+			
+			
+						<div class="col-md-3">
+				
+				<h3><a href="#">$500 Gaming PC Build</a></h3>
+				<p>
+					Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur
+					
+				</p>
+				
+				<a href="#" class="btn btn-default ">Watch Now</a>
+				
+			</div>  <!-- cl=COL 1  -->		
+			
+			
+			
+			
+			
+			
+		</div> <!-- cl=ROW  -->
+		
+		
+		
+		
+	</div>
+	
+	
+	
+
+	
+	
+	
+<!-- !END CONTENT SITE  -->	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<?php require("inc/pages/footer.php"); ?>
+	
+	
+	
+	
+<?php require("inc/pages/modal-contact.php"); ?>
+	
+	
+	
+	
+		
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="inc/js/bootstrap.js"></script>
+	
+	
+	
+	
+</body>
+</html>
